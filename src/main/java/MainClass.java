c4c91f381fa2ec2c5068f04a353c3d6f5fdf60a2
NoorZ <nuur.zreaq@gmail.com>
2021-02-23 10:54:39 +0200

    second commit

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
@@ -11,16 +10,12 @@ public class MainClass {
 
 
     public static void main (String []args){
-       /* System.out.println(Runtime.getRuntime().totalMemory()/1000000 + "  ..........    "+Runtime.getRuntime().freeMemory()+"......."+ (double)(Math.pow(2,10)));
-        System.out.printf("%.3fGiB\n", Runtime.getRuntime().maxMemory() / Math.pow(2,30) );
-        System.out.println(Runtime.getRuntime().availableProcessors());*/
+
 
 
         File dir = new File ("directory");
 
 
-        /*File [] files = dir.listFiles();
-        Arrays.stream(files).forEach(f->System.out.println(f.getName()));*/
         ForkJoinPool pool = new ForkJoinPool();
         FileCount fileCount = new FileCount(dir);
         Long current = System.currentTimeMillis();
@@ -43,34 +38,34 @@ class FileCount extends RecursiveAction {
 
 
 
-    public  void countLowerCase(File file) {
+    public  void countLowerCase(File file)  {
 
-
-        StringBuilder fileContent = new StringBuilder();
-        Scanner fileReader = null;
         try {
-            fileReader = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println(file.getName());
-        }
-        while(fileReader.hasNextLine()){
-            fileContent.append(fileReader.nextLine());
-        }
-        for (int i = 0; i < fileContent.length(); i++) {
+            FileInputStream fin = new FileInputStream(file);
+            BufferedInputStream fileReader = new BufferedInputStream(fin);
+            int c;
+            while ((c=fileReader.read() )!= -1){
 
-            if (Character.isLowerCase(fileContent.charAt(i))) {
+                if (Character.isLowerCase((char)c)) {
+
+                    try {
+                        incrementLetterAtIndex((char)c-'a');
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        //System.out.println(fileContent.charAt(i));
+                        //System.out.println(letterCount==null);
 
 
-                incrementLetterAtIndex(fileContent.charAt(i) - 'a');
-                //System.out.println("shit");
+                    }
 
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        fileReader.close();
 
 
-        //return letterCount;
+
     }
 
     @Override
@@ -81,6 +76,7 @@ class FileCount extends RecursiveAction {
         }
         else {
             File [] subFiles = file.listFiles();
+
             if(subFiles!=null&&subFiles.length >0){
                 FileCount [] fileCounts = new FileCount[subFiles.length];
                 for(int i=0;i< fileCounts.length-1;i++){
@@ -97,10 +93,7 @@ class FileCount extends RecursiveAction {
                      fileCounts[i].join();
                 }
 
-                //Integer [] joinedLetterCount = joinAllArrays(fileCounts[fileCounts.length-1].compute(), Arrays.stream(fileCounts).peek(l->l.join()).toArray(Integer[]::new));
 
-//                System.out.println(Runtime.getRuntime().availableProcessors());
-//                System.out.println(Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory());
             }
 
 
@@ -120,49 +113,4 @@ class FileCount extends RecursiveAction {
     }
 
 
-    /*@Override
-    protected int[] compute(){
-        int[] letterCount = new int[26];
-        int [] tempLetterCount ;
-        File [] files = Arrays.stream(file.listFiles()).filter(f->f.isFile()).toArray(File[]::new);
-        //System.out.println("files   "+files.length);
-        File [] dirs = Arrays.stream(file.listFiles()).filter(f->f.isDirectory()).toArray(File[]::new);
-        //System.out.println(dirs.length);
-        if(files != null&&files.length>0) {
-            for(File f:files){
-                tempLetterCount = countLowerCase(f);
-                letterCount = joinAllArrays(letterCount,tempLetterCount);
-            }
-        }
-
-        if(dirs!=null && dirs.length>0){
-            FileCount[] fileCounts = new FileCount[dirs.length];
-            for(int i=0;i< dirs.length-1;i++){
-                fileCounts[i] = new FileCount(dirs[i]);
-                fileCounts[i].fork();
-            }
-
-            fileCounts[dirs.length-1] = new FileCount(dirs[dirs.length-1]);
-            int [][] joinedLetterCount = new int[fileCounts.length][26];
-
-            joinedLetterCount[fileCounts.length -1] = joinAllArrays(fileCounts[fileCounts.length-1].compute(),letterCount);
-            for(int i=0;i< fileCounts.length-1;i++){
-                joinedLetterCount[i] = fileCounts[i].join();
-            }
-            return joinAllArrays(joinedLetterCount);
-        }
-        return letterCount;
-
-    }*/
-
-   /* private int[] joinAllArrays(int[] ... args){
-        int [] letterCounts = new int[26];
-
-        Arrays.stream(args).forEach(l-> {
-            for(int i=0;i<26;i++){
-                letterCounts[i] += l[i];
-            }
-        });
-        return letterCounts;
-    }*/
 }
